19081a364e7aab5a30797952a41e0f96b4181e76
Lucien Barcon <lucienbarcon@gmail.com>
2019-09-13 10:14:05 -0400

    Procedures pour le club + supression

--- data/procedure.sql
+++ data/procedure.sql
@@ -18,7 +18,7 @@ BEGIN
 	descriptionAncien := OLD.nom;
 	descriptionNouveau := NEW.nom;
 	description := descriptionAncien || '=>' || descriptionNouveau;
-	insert into journal(moment, operation, objet, description) VALUES(NOW(), 'edition', 'robot', 'test');
+	insert into journal(moment, operation, objet, description) VALUES(NOW(), 'edition', 'joueur', 'test');
     return NEW;
 END
 $$
@@ -42,3 +42,104 @@ CREATE TRIGGER evenementAjoutJoueur
 	BEFORE INSERT ON joueur
 		FOR EACH ROW EXECUTE PROCEDURE
 		journaliserAjoutJoueur();
+
+
+CREATE TRIGGER evenementsuprimmerclub
+    BEFORE DELETE
+    ON public.club
+    FOR EACH ROW
+EXECUTE PROCEDURE public.journalisersuprimmerclub();
+
+CREATE FUNCTION public.journalisersuprimmerjoueur()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+DECLARE
+    description text;
+BEGIN
+    description := NEW.nom;
+    insert into journal(moment, operation, objet, description) VALUES(NOW(), 'supression', 'robot', 'test');
+    return NEW;
+END
+$BODY$;
+
+ALTER FUNCTION public.journalisersuprimmerjoueur()
+    OWNER TO postgres;
+
+CREATE FUNCTION public.journaliserajoutclub()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+DECLARE
+    description text;
+BEGIN
+    description := NEW.nom;
+    insert into journal(moment, operation, objet, description) VALUES(NOW(), 'ajout', 'club', description);
+    return NEW;
+END
+$BODY$;
+
+ALTER FUNCTION public.journaliserajoutclub()
+    OWNER TO postgres;
+
+CREATE TRIGGER evenementajoutclub
+    BEFORE INSERT
+    ON public.club
+    FOR EACH ROW
+EXECUTE PROCEDURE public.journaliserajoutclub();
+
+CREATE FUNCTION public.journaliserediterclub()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+DECLARE
+    descriptionNouveau text;
+    descriptionAncien text;
+    description text;
+BEGIN
+    descriptionAncien := OLD.nom;
+    descriptionNouveau := NEW.nom;
+    description := descriptionAncien || '=>' || descriptionNouveau;
+    insert into journal(moment, operation, objet, description) VALUES(NOW(), 'edition', 'club', 'test');
+    return NEW;
+END
+$BODY$;
+
+ALTER FUNCTION public.journaliserediterclub()
+    OWNER TO postgres;
+
+CREATE TRIGGER evenementediterclub
+    BEFORE UPDATE
+    ON public.club
+    FOR EACH ROW
+EXECUTE PROCEDURE public.journaliserediterclub();
+
+CREATE FUNCTION public.journalisersuprimmerclub()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$
+DECLARE
+    description text;
+BEGIN
+    description := NEW.nom;
+    insert into journal(moment, operation, objet, description) VALUES(NOW(), 'supression', 'club', 'test');
+    return NEW;
+END
+$BODY$;
+
+ALTER FUNCTION public.journalisersuprimmerclub()
+    OWNER TO postgres;
+
+CREATE TRIGGER evenementsuprimmerclub
+    BEFORE DELETE
+    ON public.club
+    FOR EACH ROW
+EXECUTE PROCEDURE public.journalisersuprimmerclub();
